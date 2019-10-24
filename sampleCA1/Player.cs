c46ea23c12366222e9d2f240dd8c7f036a9ae39e
S00186271 <S00186271@LAB-B1041-22>
2019-10-24 12:45:44 +0100

    Add project files.

---
+++ sampleCA1/Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace sampleCA1
+{
+    class Player: IComparable
+    {
+        #region //properties
+        public int PlayerID { get; }
+        public string PlayerName { get; set; }
+        public int Score { get; private set; }
+        public static int HighScore { get; private set; }
+        #endregion //
+        public Player()
+        {
+
+        }
+        public Player(int id, string playersname, int score)
+        {
+            PlayerID = id;
+            PlayerName = playersname;
+            Score = score;
+        }
+        public void IncreaseScore (int Value)
+        {
+            if (Score <100)
+            Score = Score + Value;
+
+            if(Score > HighScore)
+            {
+                HighScore = Score;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{PlayerID}\t\t{PlayerName}\t\t{Score}";
+        }
+        public int CompareTo(object obj)
+        {
+            Player objectcomparingto = obj as Player;
+            int returnvalue = this.Score.CompareTo(objectcomparingto.Score);
+            return returnvalue;
+        }
+    }
+}
---
+++ sampleCA1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace sampleCA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Player player1 = new Player(1, "Millie", 0);
+            Player player2 = new Player(2, "Katie", 0);
+            Player player3 = new Player(3, "Kevin", 0);
+            Player player4 = new Player(4, "Conor", 0);
+            Player player5 = new Player(5, "Pauline", 0);
+
+            List<Player> allplayers = new List<Player>();
+            allplayers.Add(player1);
+            allplayers.Add(player2);
+            allplayers.Add(player3);
+            allplayers.Add(player4);
+            allplayers.Add(player5);
+
+            //player1.IncreaseScore(1);
+            //player2.IncreaseScore(10);
+
+            Display(allplayers);
+            GetScores(allplayers);
+            DisplayPlayerDetails(allplayers);
+
+            allplayers.Sort();
+            allplayers.Reverse();
+
+            WriteLine($"highest score is {Player.HighScore}");
+        }
+
+        private static void GetScores(List<Player> allplayers)
+        {
+            WriteLine("enter in number of players to add a score for =>>> ");
+            string input = ReadLine();
+            int inputNumberofPlayers = int.Parse(input);
+
+            while (inputNumberofPlayers != 0)
+            {
+                Player selectedplayer = allplayers.ElementAt(inputNumberofPlayers - 1);
+
+                selectedplayer.IncreaseScore(1);
+
+                Display(allplayers);
+
+                WriteLine("enter in number of players to add a score for =>>> ");
+                inputNumberofPlayers = int.Parse(ReadLine());
+
+            }
+        }
+
+        private static void Display(List<Player> players)
+        {
+            WriteLine("{0,-15}{1,-15}{2,-15}{3,-15}{4,-15}", "Player1", "Player2", "Player3", "Player4", "Player5");
+
+            foreach(Player player in players)
+            {
+                Write("{0,-15}",player.Score);
+            }
+
+            WriteLine();
+        }
+
+        private static void DisplayPlayerDetails(List<Player>players)
+        {
+            WriteLine($"{"playerID"}\t{"PlayerName"}\t{"Score"}");
+            foreach(Player player in players)
+            {
+                WriteLine(player);
+            }
+        }
+    }
+}
